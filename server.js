a84526e82d7fa9143a5c2baf1b64c77d33387e4e
rabatscher <p.rabatscher@hotmail.com>
2025-08-06 16:27:32 +0000

    Test 2

--- server.js
+++ server.js
@@ -16,8 +16,22 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const serviceAccountPath = path.resolve('sl-app-dc37f-firebase-adminsdk-fbsvc-ed47ed4002.json');  // Path to your service account key
-const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, 'utf8')); // Read and parse the JSON file
+// const serviceAccountPath = path.resolve('sl-app-dc37f-firebase-adminsdk-fbsvc-ed47ed4002.json');  // Path to your service account key
+// const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, 'utf8')); // Read and parse the JSON file
+
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+  universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
+};
 
 // Initialize Firebase Admin SDK with the service account
 admin.initializeApp({
